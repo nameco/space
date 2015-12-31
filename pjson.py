104d104161c5f3638ed2c055fd08ca0db18a3249
nameco <nameco@example.com>
2016-01-01 02:01:48 +0900

    introduce うすいほん:

--- pjson.py
+++ pjson.py
@@ -8,3 +8,4 @@ try:
     print json.dumps(json.loads(input_str, "utf-8"), sort_keys = True, indent = 2)
 except ValueError,e:
     sys.stderr.write("Couldn't decode \n %s \n Error : %s"%(input_str, str(e)))
+    sys.exit(e)
